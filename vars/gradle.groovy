eddc48398e85370980b5c63e170d4d01de1f0047
Christian Cordova <ccordova@yahoo.com>
2022-02-04 14:38:15 -0300

    Correcciones Gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -10,26 +10,26 @@ def call(){
     stage("Paso 2: Sonar - Análisis Estático"){
         sh "echo 'Análisis Estático!'"
         withSonarQubeEnv('sonarqube') {
-            sh './gradlew sonarqube -Dsonar.projectKey=ICLAB -Dsonar.java.binaries=build'
+            sh './gradlew sonarqube -Dsonar.projectKey=pipeline-iclab-prueba -Dsonar.java.binaries=build'
         }
     }
-    stage("Paso 3: Curl Springboot Gradle sleep 20"){
+    stage("Paso 3: Curl Springboot con Gradle durmiendo 20 segundos"){
         sh "gradle bootRun&"
-        sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
     stage("Paso 4: Subir Nexus"){
         nexusPublisher nexusInstanceId: 'nexus',
-        nexusRepositoryId: 'devops-usach-nexus',
+        nexusRepositoryId: 'pipeline-iclab-prueba',
         packages: [
             [$class: 'MavenPackage',
                 mavenAssetList: [
                     [classifier: '',
                     extension: 'jar',
-                    filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar'
+                    filePath: 'build/libs/IClabPrueba-0.0.1.jar'
                 ]
             ],
                 mavenCoordinate: [
-                    artifactId: 'DevOpsUsach2020',
+                    artifactId: 'IClabPrueba',
                     groupId: 'com.devopsusach2020',
                     packaging: 'jar',
                     version: '0.0.1'
@@ -38,13 +38,13 @@ def call(){
         ]
     }
     stage("Paso 5: Descargar Nexus"){
-        sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/pipeline-iclab-prueba/com/devopsusach2020/IClabPrueba/0.0.1/IClabPrueba-0.0.1.jar" -O'
     }
     stage("Paso 6: Levantar Artefacto Jar"){
-        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+        sh 'nohup bash java -jar IClabPrueba-0.0.1.jar & >/dev/null'
     }
-    stage("Paso 7: Testear Artefacto - Dormir"){
-        sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    stage("Paso 7: Testear Artefacto durmiendo 20 segundos"){
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
 }
 return this;
